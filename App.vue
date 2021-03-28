9a512e79fa5d1ee7536f330b78bb2fc0c005378c
Bartek Nadybal <ania.ceglarz@poczta.fm>
2021-03-28 10:26:49 +0200

    Initial Vue.js.project

---
+++ App.vue
@@ -0,0 +1,79 @@
+
+<template>
+<div class="container-fluid p-0 m-0 vh-100">
+<div class="row p-0 m-0 h-100">
+<div class="col-12 col-md-5 col-lg-4 bg-light p-0 h-100">
+<h3 class="w-100 text-center p-4">
+<i class="bi bi-clipboard-check text-secondary"></i>
+GoNote
+</h3>
+<button type="button" class="btn btn-dark w-100 py-2 rounded-0">
+<i class="bi bi-pen-fill"></i>
+Stwórz nową notatkę
+</button>
+<div class="alert alert-secondary m-3 text-center">
+<i class="bi bi-emoji-frown"></i>
+Jeszcze nie ma żadnych notatek do wyświetlenia
+</div>
+<div class="list-group pt-3 pb-5 px-1">
+<div v-for="note in notes" v-bind:key="note" class="list-group-item">
+<div class="float-end">
+<div class="btn-group" role="group">
+<button type="button" class="btn btn-outline-danger">
+<i class="bi bi-trash-fill"></i>
+</button>
+<button type="button" class="btn btn-outline-secondary">
+<i class="bi bi-eye-fill"></i>
+</button>
+</div>
+</div>
+<h5 class="mb-1">{{ note.name }}</h5>
+<p class="text-muted">Utworzono 14:15 10.03.2021</p>
+</div>
+</div>
+</div>
+<div class="col-12 col-md-7 col-lg-8 bg-white p-0">
+<div class="cover d-none d-sm-block"></div>
+<note-add-form v-on:onAddNote="addNote($event)"></note-add-form>
+</div>
+</div>
+</div>
+<nav class="navbar fixed-bottom navbar-dark bg-dark">
+<div class="container-fluid justify-content-end p-2">
+<span class="text-muted mx-5 d-none d-md-block">
+©2021 Polityka prywatności | Warunki korzystania z usługi
+</span>
+<span class="text-light">
+<i class="bi bi-heart-fill"></i>
+Moje konto
+</span>
+</div>
+</nav>
+</template>
+
+<script>
+import NoteAddForm from "./components/NoteAddForm.vue";
+
+export default {
+components: {NoteAddForm},
+data: function (){
+return {
+notes: [],
+}
+},
+methods: {
+addNote: function (note){
+this.notes.push(note);
+},
+},
+}
+</script>
+
+<style>
+.cover {
+background-image: url("./assets/background.jpg");
+background-position: center;
+background-size: cover;
+min-height: 150px;
+}
+</style>
